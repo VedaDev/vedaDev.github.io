3aff5054e8db9c6f5a37019ce036388b431787f5
david <davidyang@inquartik.com>
2021-06-11 02:37:41 +0800

    remove comments

--- src/gatsby-theme-blog/components/bio-content.js
+++ src/gatsby-theme-blog/components/bio-content.js
@@ -8,12 +8,16 @@ import { Themed } from "theme-ui"
 export default function Bio() {
   return (
     <>
-      /* This is where <Themed.a href="http://example.com/">your name</Themed.a> */
+      /* 
+       *This is where <Themed.a href="http://example.com/">your name</Themed.a>
+       */
 	If you can’t fly, then run, if you can’t run then walk if you can’t walk then crawl, but whatever you do you have to keep moving forward
       {` `}
       goes.
       <br />
-      Or whatever, you make the rules.
+      /*
+       *Or whatever, you make the rules.
+       */
     </>
   )
 }
